7f16713b27e879628981d9fcf22781e61637f9a4
ROHIT BANSAL <143306289+ROHITBANSAL65@users.noreply.github.com>
2025-07-22 11:20:57 +0530

    Reddit-main/Reddit-main directory

--- Reddit-main/Reddit-main/src/app/(protected)/(tabs)/communities.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function CommunitiesScreen() {
-  return (
-    <View>
-      <Text>Communities Screen</Text>
-    </View>
-  )
-}
--- Reddit-main/Reddit-main/src/atoms.tsx
+++
@@ -1,6 +0,0 @@
-import { atom } from "jotai";
-import { Tables } from "./types/database.types";
-
-type Group = Tables<"groups">;
-
-export const selectedGroupAtom = atom<Group | null>(null);
